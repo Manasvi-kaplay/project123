208856c7ce4d7f6cd1f7848d99b38e484ae9c4b8
Manasvi Kaplay <manasvi111.kaplay@gmail.com>
2020-12-22 23:01:54 -1200

    23dec

--- controller/payment_gateway.js
+++ controller/payment_gateway.js
@@ -6,6 +6,52 @@ router.get('/pay',function(req,res){
   var pagedata={"title":"Payments","pagename":"payments"}
   res.render("layout",pagedata);
 })
+
+router.post('/refund',function(req,res){
+  var email=req.body.email;
+  allQueries.find("users",{email:email},function(err0,result0){
+    if(err0){
+      res.status(400).json({status:0,error:err0});
+    }
+    if(result0){
+      var custArrLen=result0[0].customer_id.length;
+      var custId=result0[0].customer_id[custArrLen-1];
+      console.log("custId..",custId);
+      stripe.invoices.list({customer:custId},function(err,result){
+        if(err){
+          res.status(400).json({status:0,error:err});
+        }
+        if(result){
+//           let date_ob = new Date();
+// let date = ("0" + date_ob.getDate()).slice(-2);
+// let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+// let year = date_ob.getFullYear();
+// let hours = date_ob.getHours();
+// let minutes = date_ob.getMinutes();
+// let seconds = date_ob.getSeconds();
+// var date_time=year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
+// console.log("today's date...",date_time);
+          // var pi=result.data[0].payment_intent;
+          // console.log("pi..",pi);
+          var subscriptionid=result.data[0].subscription;
+          // var planStartDt=result0[0].payment_date[custArrLen-1];
+          // console.log("planStartDt..",planStartDt);
+          // const diffInMs   = new Date(date_time) - new Date(planStartDt);
+          // const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+          // console.log("diff in days..",diffInDays);
+          stripe.subscriptions.del(subscriptionid,{prorate:true},function(err2,result2){
+            if(err2){
+              res.status(400).json({status:0,error:err2});
+            }
+            if(result2){
+              res.status(200).json({status:1,result2:result2});
+            }
+          })
+        }
+      })
+    }
+  }) 
+})
 router.post('/create',function(req,res){
   console.log("req.body..",req.body);
   // Elements to be sent by the front end:
@@ -94,17 +140,26 @@ var details=[];
 var detailsObj={};
 var inv;
 async function retrieve(){
+  try{
   for(var i=0;i<result[0].customer_id.length;i+=1){
    inv= await stripe.invoices.retrieveUpcoming({customer:result[0].customer_id[i]})
     detailsObj={"amount":inv.lines.data[0].amount/100,"plan":inv.lines.data[0].description,"payment_date":result[0].payment_date[i]}
-    //console.log("detailsObj..",detailsObj);
+    console.log("inv..",inv);
     details.push(detailsObj);
   }
   res.status(200).json({status:1,payment_history:details});
 }
+catch(error){
+  res.status(400).json({status:0,error:error});
+}
+}
+
 retrieve();
 }
   })
+})
+router.get('/rere',function(req,res){
+  res.send("Hello world...");
 })
 
 module.exports=router;
